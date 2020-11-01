0231b2d6f482f0646d4359a59a8696474401c0e2
Febro Herdyanto <febroherdyanto@outlook.com>
2020-11-01 14:29:50 +0700

    Creating index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+
+<head>
+   <title>Sistem Informasi Pengumpulan Tugas</title>
+</head>
+
+<body>
+
+<center><h1>Selamat Datang di Sistem Informasi Pengumpulan Tugas<h1>
+<h2>TI.20.B.1<h2>
+<h3>Teknik Informatika - Universitas Pelita Bangsa<h3></center>
+
+</body>
+
+<html>
