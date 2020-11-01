3910360909229e474e52e3cf2330884c0fe148c2
Febro Herdyanto <febroherdyanto@outlook.com>
2020-11-01 14:51:18 +0700

    Update admin page

---
+++ xadmin/index.php
@@ -0,0 +1,1 @@
+<h1>Selamat Datang di Halaman Admin<h1>
---
+++ xconfig/xconfig.php
@@ -0,0 +1,9 @@
+<?php
+
+$koneksi = mysqli("localhost","root","raspberry","tugas_upb");
+
+if(mysqli_connect_errno(){
+	echo "Connection Error, because : ".mysqli_connect_error();
+}
+
+?>
